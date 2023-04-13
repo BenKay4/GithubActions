e13a263748474beda97882175c4866fa3cac2180
BenKay4 <108741623+BenKay4@users.noreply.github.com>
2023-04-12 23:30:34 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,101 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
-    }
+         [Test]
+        public void Add_Valid()
+        {
+            Assert.AreEqual(3, Program.Add("1", "2"));
+            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
+        }
+
+        [Test]
+        public void Add_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Add_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Subtract_Valid_Kayigamba()
+        {
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-1, Program.Subtract("2", "3"));
+            Assert.AreEqual(0, Program.Subtract("5", "5"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Kayigamba()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Kayigamba()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Kayigamba()
+        {
+            Assert.AreEqual(6, Program.Multiply("2", "3"));
+            Assert.AreEqual(-6, Program.Multiply("-2", "3"));
+            Assert.AreEqual(0, Program.Multiply("0", "5"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Kayigamba()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Kayigamba()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Kayigamba()
+        {
+            Assert.AreEqual(5, Program.Divide("10", "2"));
+            Assert.AreEqual(0.5, Program.Divide("1", "2"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Kayigamba()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("a", "2"));
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("10 2", "2"));
+            Assert.Throws<FormatException>(() => Program.Divide("10", "0"));
+        }
+
+        [Test]
+        public void Divide_Null_Kayigamba()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
 }
